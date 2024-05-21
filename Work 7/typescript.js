59325c2c39b85dfc648dff8470097a0d2eda2800
Berkhan <123936730+nahkreb@users.noreply.github.com>
2024-05-22 00:08:50 +0300

    Work 7

---
+++ Work 7/typescript.js
@@ -0,0 +1,35 @@
+function gerisayim() {
+    var sayac = 35; // toplam süre
+    var countElement = document.getElementById('skipButton');
+    var progressBar = document.getElementById('progress');
+    var totalTime = sayac;
+    var interval = 1000;
+    var elapsed = 0; // geçen süre
+    if (!countElement || !progressBar) {
+        console.error('Elementler bulunamadı!');
+        return;
+    }
+    var intervalId = window.setInterval(function () {
+        elapsed++;
+        countElement.innerText = "Reklam\u0131n Bitmesi i\u00E7in: ".concat(totalTime - elapsed);
+        var progressPercentage = (elapsed / totalTime) * 100;
+        progressBar.style.width = progressPercentage + '%';
+        if (elapsed >= totalTime) {
+            clearInterval(intervalId);
+            countElement.innerText = "Reklam Bitti";
+            closeAd();
+        }
+    }, interval);
+}
+function closeAd() {
+    var adPopup = document.getElementById('ad-popup');
+    var blurBackground = document.getElementById('blur-background');
+    if (adPopup && blurBackground) {
+        adPopup.style.display = 'none';
+        blurBackground.style.display = 'none';
+    }
+    else {
+        console.error('Elementler bulunamadı!');
+    }
+}
+gerisayim();
---
+++ Work 7/typescript.ts
@@ -0,0 +1,40 @@
+function gerisayim(): void {
+    const sayac: number = 35; // toplam süre
+    const countElement: HTMLElement | null = document.getElementById('skipButton');
+    const progressBar: HTMLElement | null = document.getElementById('progress');
+    const totalTime: number = sayac;
+    const interval: number = 1000;
+    let elapsed: number = 0; // geçen süre
+
+    if (!countElement || !progressBar) {
+        console.error('Elementler bulunamadı!');
+        return;
+    }
+
+    const intervalId: number = window.setInterval((): void => {
+        elapsed++;
+        countElement.innerText = `Reklamın Bitmesi için: ${totalTime - elapsed}`;
+        const progressPercentage: number = (elapsed / totalTime) * 100;
+        progressBar.style.width = progressPercentage + '%';
+
+        if (elapsed >= totalTime) {
+            clearInterval(intervalId);
+            countElement.innerText = "Reklam Bitti";
+            closeAd();
+        }
+    }, interval);
+}
+
+function closeAd(): void {
+    const adPopup: HTMLElement | null = document.getElementById('ad-popup');
+    const blurBackground: HTMLElement | null = document.getElementById('blur-background');
+
+    if (adPopup && blurBackground) {
+        adPopup.style.display = 'none';
+        blurBackground.style.display = 'none';
+    } else {
+        console.error('Elementler bulunamadı!');
+    }
+}
+
+gerisayim();
